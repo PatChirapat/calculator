8e6ea06acc9399e423a9e040ec819a094e8f19cb
Chirapat <chirapat.ch@ku.th>
2024-02-19 21:20:07 +0700

    Completed main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+"""main.py"""
 from calculator_controller import CalculatorController
 
 if __name__ == '__main__':
+    """display use interface"""
     app = CalculatorController()
     app.view.mainloop()
