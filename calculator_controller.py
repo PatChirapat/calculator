6e4aee74737834b3c0b80df7d861168f14238312
Chirapat <chirapat.ch@ku.th>
2024-02-19 20:17:16 +0700

    edit controller

--- calculator_controller.py
+++ calculator_controller.py
@@ -1,6 +1,7 @@
 from calculator_model import CalculatorModel
 from calculator_view import CalculatorView
 
+
 class CalculatorController:
     def __init__(self):
         self.model = CalculatorModel()
@@ -11,9 +12,13 @@ class CalculatorController:
             self.model.add_display(key)
             self.view.display_text.set(self.model.current_display)
         else:
-            self.model.evaluate_result()
-            self.view.display_text.set(self.model.current_display)
-            self.view.display_history('\n'.join(self.model.history))
+            if self.model.current_display and self.model.current_display[0] in self.view.function_combobox['values']:
+                selected_function = self.view.function_combobox.get()
+                self.on_function_evaluate(selected_function)
+            else:
+                self.model.evaluate_result()
+                self.view.display_text.set(self.model.current_display)
+                self.view.display_history('\n'.join(self.model.history))
 
     def on_operator_evaluate(self, operator):
         if operator == "DEL":
@@ -27,10 +32,18 @@ class CalculatorController:
             self.model.evaluate_result()
         else:
             self.model.add_display(operator)
+
         self.view.display_text.set(self.model.current_display)
 
     def on_function_evaluate(self, function):
-        self.model.calculate_function(function)
-        self.view.display_text.set(self.model.current_display)
+        current_display = self.model.current_display
 
+        if current_display:
+            display_text = f"{function}({current_display})"
+            self.view.display_text.set(display_text)
 
+            result = self.model.calculate_function(function)
+
+            history_entry = f"{function}({current_display}) = {result}"
+            self.model.history.append(history_entry)
+            self.view.display_history('\n'.join(self.model.history))
